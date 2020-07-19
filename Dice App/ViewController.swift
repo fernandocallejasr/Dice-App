5bc99dfb9c13eef336562089f6160751ce73b32a
Fernando Callejas <ferd.callejas@gmail.com>
2020-07-18 19:41:49 -0500

    Initial Commit

---
+++ Dice App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice App
+//
+//  Created by user177767 on 7/18/20.
+//  Copyright © 2020 Fernando Callejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
