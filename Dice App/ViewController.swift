91ecea4c1f5278899339b174eeeac49eb7e5c814
Fernando Callejas <ferd.callejas@gmail.com>
2020-07-18 20:45:15 -0500

    Random Functionality
    Added random functionality for dice views when tapping the roll button

--- Dice App/ViewController.swift
+++ Dice App/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceView1: UIImageView!
+    @IBOutlet weak var diceView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceView1.alpha = 0.5
+        diceView2.alpha = 0.5
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        diceView1.alpha = 1
+        diceView2.alpha = 1     
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceView1.image = diceArray.randomElement()
+        diceView2.image = diceArray.randomElement()
+    }
+    
 }
 
